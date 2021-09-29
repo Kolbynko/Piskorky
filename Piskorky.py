b2aedffe29155aebad2a4b37db2deccdd4cdf0b9
Kolbynko <90548034+Kolbynko@users.noreply.github.com>
2021-09-29 09:34:50 +0200

    Nefungujuci koncept

---
+++ Piskorky.py
@@ -0,0 +1,12 @@
+#piskorky
+#strana a pola
+#a=int(input('Zadaj dlzku pola: '))
+
+a=3
+pos=[]
+hodnota=[]
+
+    for i in range(1,a+1):
+        pos.append(int(input('Zadaj poziciu: ')))
+        hodnota.append(str(input('Zadaj znak: ')))
+
